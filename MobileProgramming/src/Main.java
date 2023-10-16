96ee4e9da9a3d36ab6324eec44822e94dd3ff8e1
giovannijunseokim <kingjunseo@gmail.com>
2023-10-16 18:30:13 +0900

    [chore] lab4

--- MobileProgramming/src/Main.java
+++ MobileProgramming/src/Main.java
@@ -1,134 +1,232 @@
-import java.util.Arrays; // for main3
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        main1(args);
-        main2(args);
-        main3(args);
-        main4(args);
-        main5(args);
-    }
+    // 테트리스 조각들
+    private static int[][][][] setOfBlockArrays = { // [7][4][?][?]
+            {
+                    {
+                            {1, 1},
+                            {1, 1}
+                    },
+                    {
+                            {1, 1},
+                            {1, 1}
+                    },
+                    {
+                            {1, 1},
+                            {1, 1}
+                    },
+                    {
+                            {1, 1},
+                            {1, 1}
+                    }
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {0, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {1, 0, 0},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 1},
+                            {0, 1, 0},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 0},
+                            {1, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 0, 1},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 0},
+                            {0, 1, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 0},
+                            {0, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 1, 1},
+                            {1, 1, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 1, 1},
+                            {1, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                    },
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                    },
+            },
+    }; // end of setOfBlockArrays
+    // 입력받는 객체
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // 입력 String
+    private static String line = null;
+    // String 내부 char의 개수
+    private static int nKeys = 0;
 
-    public static void main1(String[] args) {
-        String s1 = "Hello, Java!";
-        System.out.print("s1="); System.out.println(s1);
-        String s2 = String.format("%s, %s!", "Hello", "Java");
-        System.out.print("s2="); System.out.println(s2);
-
-        int len1 = s1.length();
-        int len2 = s2.length();
-        System.out.println("len1=" + len1);
-        System.out.println("len2=" + len2);
-
-        boolean b1 = s1.equals(s1); // compare s1 and s2 (in content)
-        boolean b2 = s1.equals(s2); // compare s1 and s2 (in content)
-        boolean b3 = (s1 == s2); // compare s1 and s2 (in objects)
-
-        System.out.println("s1.equals(s1)=" + b1);
-        System.out.println("s1.equals(s2)=" + b2);
-        System.out.println("(s1==s2)=" +  b3);
-    }
-
-    public static void main2(String[] args) {
-        String istr = "1234";
-        String dstr = "12.34";
-
-        int ival = Integer.parseInt(istr); // string -> integer value
-        double dval = Double.parseDouble(dstr); // string -> double value
-        System.out.println("before : " + ival + ", " + dval);
-
-        ival = ival + 1111;
-        dval = dval + 11.11;
-        String istr2 = String.valueOf(ival); // integer value -> string
-        String dstr2 = String.valueOf(dval); // double value -> string
-
-        System.out.println("after  : " + istr2 + ", " + dstr2);
-    }
-
-    public static void main3(String[] args) {
-        int A1[] = null;
-        int A2[] = {1, 2, 3, 4, 5};
-        int[] A3 = new int[5];
-        int A4[] = new int[]{1, 2, 3, 4, 5};
-
-        System.out.print("A1:");
-        printArray(A1);
-        System.out.print("A2:");
-        printArray(A2);
-        System.out.print("A3:");
-        printArray(A3);
-        System.out.print("A4:");
-        printArray(A4);
-        System.out.println("A2.equals(A3)=" + A2.equals(A3));
-        System.out.println("A2.equals(A4)=" + A2.equals(A4));
-        System.out.println("Arrays.equals(A2, A3)=" + Arrays.equals(A2, A3));
-        System.out.println("Arrays.equals(A2, A4)=" + Arrays.equals(A2, A4));
-    }
-
-    public static void printArray(int a[]) {
-        if (a != null) {
-            for (int i = 0; i < a.length; i++)
-                System.out.print(a[i] + " ");
+    // 입력 키 받기
+    private static char getKey() throws IOException {
+        char ch;
+        // 아직 입력받은 String이 있음.
+        if (nKeys != 0) {
+            // 맨 첫 번째(아직 안 쓴) 사용
+            ch = line.charAt(line.length() - nKeys);
+            nKeys--;
+            return ch;
         }
-        System.out.println();
+        do {
+            line = br.readLine();
+            nKeys = line.length();
+        } while (nKeys == 0);
+        // String의 맨 첫번째 char
+        ch = line.charAt(0);
+        // char 하나 줄음
+        nKeys--;
+        return ch;
     }
 
-    public static void main4(String[] args) {
-        Nested m1 = new Nested(1, 2);
-        Nested m2 = new Nested(3, 4);
-        System.out.println("m1.get_dy()=" + m1.get_dy() + ", m1.get_dx()=" + m1.get_dx());
-        System.out.println("m2.get_dy()=" + m2.get_dy() + ", m2.get_dx()=" + m2.get_dx());
-        Nested.InnerD d1 = m1.new InnerD();
-        Nested.InnerS s1 = new Nested.InnerS();
-        Nested.InnerD d2  = m2.new InnerD();
-        Nested.InnerS s2 = new Nested.InnerS();
-        System.out.println("d1.get_dy()=" + d1.get_dy() + ", s1.get_dx()=" + s1.get_dx() + ", d1.sum()=" + d1.sum());
-        System.out.println("d2.get_dy()=" + d2.get_dy() + ", s2.get_dx()=" + s2.get_dx() + ", d2.sum()=" + d2.sum());
-    }
-
-    public static void main5(String[] args) {
-        Matrix m1 = new Matrix(3,3);
-        m1.print(); System.out.println();
-        int A[][] = { { 0, 1, 0, }, { 1, 1, 1, }, { 0, 0, 0, } };
-        Matrix m2 = new Matrix(A);
-        m2.print(); System.out.println();
-        MyMatrix m3 = new MyMatrix(3,3);
-        m3.print(); System.out.println();
-        MyMatrix m4 = new MyMatrix(A);
-        m4.print(); System.out.println();
-        m2 = m4; // polymorphism: Matrix covers MyMatrix!!
-        m2.print(); System.out.println();
-    }
-}
-
-class Nested {
-    private int dy; // dynamic variable
-    private static int dx; // static variable
-    public int get_dy() { return dy; }
-    public static int get_dx() { return dx; } // can be declared 'dynamic'
-    public Nested(int cy, int cx) { dy = cy; dx = cx; }
-    public class InnerD {
-        public int get_dy() { return dy; }
-        public int sum() { return dy+dx; }
-    }
-    public static class InnerS {  public int get_dx() { return dx; }  }
-}
-
-class MyMatrix extends Matrix {
-    public MyMatrix() { super(); }
-    public MyMatrix(int cy, int cx) { super(cy, cx); }
-    public MyMatrix(int[][] a) { super(a); }
-    public void print() {
-        int dy = get_dy();
-        int dx = get_dx();
-        int array[][] = get_array();
-        for (int y=0; y < dy; y++) {
-            for (int x=0; x < dx; x++) {
+    // 0이면 □, 1이면 ■로 그림
+    public static void drawMatrix(Matrix m) {
+        int dy = m.get_dy();
+        int dx = m.get_dx();
+        int array[][] = m.get_array();
+        for (int y = 0; y < dy; y++) {
+            for (int x = 0; x < dx; x++) {
                 if (array[y][x] == 0) System.out.print("□ ");
-                else if (get_array()[y][x] == 1) System.out.print("■ ");
+                else if (array[y][x] == 1) System.out.print("■ ");
                 else System.out.print("X ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        char key;
+        TetrisState state;
+        Tetris.init(setOfBlockArrays);
+        Tetris board = new Tetris(15, 10);
+        Random random = new Random();
+        key = (char) ('0' + random.nextInt(7));
+        board.accept(key);
+        drawMatrix(board.get_oScreen());
+        System.out.println();
+
+        while ((key = getKey()) != 'q') {
+            state = board.accept(key);
+            drawMatrix(board.get_oScreen());
+            System.out.println();
+            if (state == TetrisState.NewBlock) {
+                key = (char) ('0' + random.nextInt(7));
+                state = board.accept(key);
+                drawMatrix(board.get_oScreen());
+                System.out.println();
+                if (state == TetrisState.Finished) break; // Game Over!
+            }
+        }
+        System.out.println("Program terminated!");
     }
 }
--- MobileProgramming/src/Matrix.java
+++ MobileProgramming/src/Matrix.java
@@ -1,124 +1,164 @@
-
 public class Matrix {
     private static int nAlloc = 0;
     private static int nFree = 0;
+
+    // free = 해제
     protected void finalize() throws Throwable {
         super.finalize();
         nFree++; // count the number of freed objects
     }
-    public int get_nAlloc() { return nAlloc; }
-    public int get_nFree() { return nFree; }
+
+    public int get_nAlloc() {
+        return nAlloc;
+    }
+
+    public int get_nFree() {
+        return nFree;
+    }
 
     private int dy = 0;
     private int dx = 0;
     private int[][] array = null;
-    public int get_dy() { return dy; }
-    public int get_dx() { return dx; }
-    public int[][] get_array() { return array; }
-    private void alloc(int cy, int cx) {
-        if((cy < 0) || (cx < 0)) {
-            System.out.println("wrong matrix size");
-            System.exit(0);
-        }
+
+    public int get_dy() {
+        return dy;
+    }
+
+    public int get_dx() {
+        return dx;
+    }
+
+    public int[][] get_array() {
+        return array;
+    }
+
+    // alloc = 할당
+    private void alloc(int cy, int cx) throws MatrixException {
+        if ((cy < 0) || (cx < 0))
+            throw new MatrixException("wrong matrix size");
         dy = cy;
         dx = cx;
         array = new int[dy][dx];
         nAlloc++; // count the number of allocated objects
     }
-    public Matrix() {  alloc(0, 0);	}
-    public Matrix(int cy, int cx) {
+
+    public Matrix() throws MatrixException {
+        alloc(0, 0);
+    }
+
+    public Matrix(int cy, int cx) throws MatrixException {
         alloc(cy, cx);
-        for(int y = 0; y < dy; y++)
-            for(int x = 0; x < dx; x++)
+        for (int y = 0; y < dy; y++)
+            for (int x = 0; x < dx; x++)
                 array[y][x] = 0;
     }
-    public Matrix(Matrix obj) {
+
+    public Matrix(Matrix obj) throws MatrixException {
         alloc(obj.dy, obj.dx);
-        for(int y = 0; y < dy; y++)
-            for(int x = 0; x < dx; x++)
+        for (int y = 0; y < dy; y++)
+            for (int x = 0; x < dx; x++)
                 array[y][x] = obj.array[y][x];
     }
-    public Matrix(int[][] a) {
+
+    public Matrix(int[][] a) throws MatrixException {
         alloc(a.length, a[0].length);
-        for(int y = 0; y < dy; y++)
-            for(int x = 0; x < dx; x++)
+        for (int y = 0; y < dy; y++)
+            for (int x = 0; x < dx; x++)
                 array[y][x] = a[y][x];
     }
-    public Matrix clip(int top, int left, int bottom, int right) {
+
+    // 일부 잘라내기
+    public Matrix clip(int top, int left, int bottom, int right) throws MatrixException {
         int cy = bottom - top;
         int cx = right - left;
         Matrix temp = new Matrix(cy, cx);
-        for(int y = 0; y < cy; y++){
-            for(int x = 0; x < cx; x++){
-                if((top+y >= 0) && (left+x >= 0) &&	(top+y < dy) && (left+x < dx))
-                    temp.array[y][x] = array[top+y][left+x];
-                else {
-                    System.out.println("invalid matrix range");
-                    System.exit(0);
-                }
+        for (int y = 0; y < cy; y++) {
+            for (int x = 0; x < cx; x++) {
+                if ((top + y >= 0) && (left + x >= 0) && (top + y < dy) && (left + x < dx))
+                    temp.array[y][x] = array[top + y][left + x];
+                else
+                    throw new MatrixException("invalid matrix range");
             }
         }
         return temp;
     }
-    public void paste(Matrix obj, int top, int left) {
-        for(int y = 0; y < obj.dy; y++)
-            for(int x = 0; x < obj.dx; x++) {
-                if((top+y >= 0) && (left+x >= 0) &&	(top+y < dy) && (left+x < dx))
+
+    // (top, left)에 매트릭스 붙여넣기
+    public void paste(Matrix obj, int top, int left) throws MatrixException {
+        for (int y = 0; y < obj.dy; y++)
+            for (int x = 0; x < obj.dx; x++) {
+                if ((top + y >= 0) && (left + x >= 0) && (top + y < dy) && (left + x < dx))
                     array[y + top][x + left] = obj.array[y][x];
-                else {
-                    System.out.println("invalid matrix range");
-                    System.exit(0);
-                }
+                else
+                    throw new MatrixException("invalid matrix range");
             }
     }
-    public Matrix add(Matrix obj) {
-        if((dx != obj.dx) || (dy != obj.dy)) {
-            System.out.println("matrix sizes mismatch");
-            System.exit(0);
-        }
+
+    // 두 매트릭스 합함
+    public Matrix add(Matrix obj) throws MatrixException {
+        if ((dx != obj.dx) || (dy != obj.dy))
+            throw new MismatchedMatrixException("matrix sizes mismatch");
         Matrix temp = new Matrix(dy, dx);
-        for(int y = 0; y < obj.dy; y++)
-            for(int x = 0; x < obj.dx; x++)
+        for (int y = 0; y < obj.dy; y++)
+            for (int x = 0; x < obj.dx; x++)
                 temp.array[y][x] = array[y][x] + obj.array[y][x];
         return temp;
     }
-    public int sum(){
+
+    // 매트릭스 내의 총합
+    public int sum() {
         int total = 0;
-        for(int y = 0; y < dy; y++)
-            for(int x = 0; x < dx; x++)
+        for (int y = 0; y < dy; y++)
+            for (int x = 0; x < dx; x++)
                 total += array[y][x];
         return total;
     }
-    public void mulc(int coef){
-        for(int y = 0; y < dy; y++)
-            for(int x = 0; x < dx; x++)
-                array[y][x] = coef * array[y][x];
-    }
-    Matrix int2bool() {
-        Matrix temp = new Matrix(dy, dx);
-        int[][] t_array = temp.get_array();
+
+    // 매트릭스 내의 값 곱셈
+    public void mulc(int coef) {
         for (int y = 0; y < dy; y++)
             for (int x = 0; x < dx; x++)
-                t_array[y][x] = (array[y][x] != 0 ? 1 : 0);
-        return temp;
+                array[y][x] = coef * array[y][x];
     }
-    public boolean anyGreaterThan(int val){
-        for(int y = 0; y < array.length; y++){
-            for(int x = 0; x < array[0].length ; x++){
+
+    // val보다 큰 값이 있는가?
+    public boolean anyGreaterThan(int val) {
+        for (int y = 0; y < array.length; y++) {
+            for (int x = 0; x < array[0].length; x++) {
                 if (array[y][x] > val) return true;
             }
         }
         return false;
     }
-    public void print(){
+
+    // 매트릭스 출력
+    public void print() {
         System.out.println("Matrix(" + dy + "," + dx + ")");
-        for(int y = 0; y < dy; y++) {
-            for(int x = 0; x < dx; x++)
-                System.out.print(array[y][x]+" ");
+        for (int y = 0; y < dy; y++) {
+            for (int x = 0; x < dx; x++)
+                System.out.print(array[y][x] + " ");
             System.out.println();
         }
     }
     // end of Matrix
 }
 
+class MatrixException extends Exception {
+    public MatrixException() {
+        super("Matrix Exception");
+    }
 
+    public MatrixException(String msg) {
+        super(msg);
+    }
+}
+
+class MismatchedMatrixException extends MatrixException {
+    public MismatchedMatrixException() {
+        super("Mismatched Matrix Exception");
+    }
+
+    public MismatchedMatrixException(String msg) {
+        super(msg);
+    }
+}
